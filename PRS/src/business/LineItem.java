f2dc93f093eb6107afdf20a2246d3db7e643a887
mchilders14 <childersmatt4@gmail.com>
2021-06-28 14:03:05 -0400

    adding prs POJO

--- PRS/src/business/LineItem.java
+++ PRS/src/business/LineItem.java
@@ -1,5 +1,68 @@
 package business;
 
 public class LineItem {
+	
+	private int id;
+	private Request request;
+	private Product product;
+	private int quantity;
+	
+	public LineItem() {
+		// Default empty constructor
+	}
+
+	public LineItem(int id, Request request, Product product, int quantity) {
+		super();
+		this.id = id;
+		this.request = request;
+		this.product = product;
+		this.quantity = quantity;
+	}
+
+	public LineItem(Request request, Product product, int quantity) {
+		super();
+		this.request = request;
+		this.product = product;
+		this.quantity = quantity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Request getRequest() {
+		return request;
+	}
+
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "LineItem [id=" + id + ", request=" + request + ", product=" + product + ", quantity=" + quantity + "]";
+	}
+	
+	
 
 }
