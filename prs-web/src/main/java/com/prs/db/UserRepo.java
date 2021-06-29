41d9cb751cef9755b60bff3038637b853486d2c0
mchilders14 <childersmatt4@gmail.com>
2021-06-29 11:55:05 -0400

    adding prs web app

---
+++ prs-web/src/main/java/com/prs/db/UserRepo.java
@@ -0,0 +1,11 @@
+package com.prs.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.prs.business.User;
+
+public interface UserRepo extends CrudRepository<User, Integer>{	// If interface uses interface, MUST extend.
+	
+	
+
+}
