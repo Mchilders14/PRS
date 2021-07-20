a863de31ec66d85b294c676725d547eef8e04e02
mchilders14 <childersmatt4@gmail.com>
2021-07-20 15:35:18 -0400

    Adding comment

--- prs-web/src/main/java/com/prs/PrsWebApplication.java
+++ prs-web/src/main/java/com/prs/PrsWebApplication.java
@@ -3,6 +3,7 @@ package com.prs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Running Spring Application
 @SpringBootApplication
 public class PrsWebApplication {
 
